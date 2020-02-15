256f069200dc38aeb7c77ec9f261ee1756788362
Ryan Hoffmann <ryan@hoff.pub>
2020-02-14 21:59:36 -0600

    feat(schedule): create schedule

---
+++ README.md
@@ -0,0 +1,2 @@
+# Material UI Schedule
+Customizable Schedule for Material UI.
---
+++ src/components/Schedule/Schedule.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Schedule() {
+  return (
+    <span>schedule</span>
+  );
+}
+
+export default Schedule;
